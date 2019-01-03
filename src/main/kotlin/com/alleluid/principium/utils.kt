665dce2de2cd28a8d998a86ab795ec09ed2e4826
Alleluid <alleluid@users.noreply.github.com>
2019-01-02 19:06:51 -0800

    Added "enum" for MC formatting codes

--- src/main/kotlin/com/alleluid/principium/utils.kt
+++ src/main/kotlin/com/alleluid/principium/utils.kt
@@ -30,4 +30,33 @@ object Utils {
     fun Entity.setPositionAndRotationAndUpdate(x: Double, y: Double, z: Double) {
         this.setPositionAndRotationAndUpdate(x, y, z, this.rotationYaw, this.rotationPitch)
     }
+
+    object Formatting{
+        // Reference: https://minecraft.gamepedia.com/Formatting_codes
+        // Colors
+        const val BLACK = "§0"
+        const val DARK_BLUE = "§1"
+        const val DARK_GREEN = "§2"
+        const val DARK_AQUA = "§3"
+        const val DARK_RED = "§4"
+        const val DARK_PURPLE = "§5"
+        const val GOLD = "§6"
+        const val GREY = "§7"
+        const val DARK_GREY = "§8"
+        const val BLUE = "§9"
+        const val GREEN = "§a"
+        const val AQUA = "§b"
+        const val RED = "§c"
+        const val LIGHT_PURPLE = "§d"
+        const val YELLOW = "§e"
+        const val WHITE = "§f"
+
+        // Styles
+        const val OBFUSCATED = "§k"
+        const val BOLD = "§l"
+        const val STRIKETHROUGH = "§m"
+        const val UNDERLINE = "§n"
+        const val ITALIC = "§o"
+        const val RESET = "§r"
+    }
 }
