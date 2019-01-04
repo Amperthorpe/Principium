16068c5868af82ac6cf0884d38ab811e90e72ade
Alleluid <alleluid@users.noreply.github.com>
2019-01-04 11:02:57 -0800

    Registers TE Pedestal

--- src/main/kotlin/com/alleluid/principium/common/blocks/pedestal/TileEntityPedestal.kt
+++ src/main/kotlin/com/alleluid/principium/common/blocks/pedestal/TileEntityPedestal.kt
@@ -1,16 +1,30 @@
 package com.alleluid.principium.common.blocks.pedestal
 
+import com.alleluid.principium.MOD_ID
+import com.alleluid.principium.Utils
 import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.util.EnumFacing
+import net.minecraft.util.ResourceLocation
 import net.minecraftforge.common.capabilities.Capability
+import net.minecraftforge.fml.common.registry.GameRegistry
 import net.minecraftforge.items.CapabilityItemHandler
 import net.minecraftforge.items.IItemHandler
 import net.minecraftforge.items.ItemStackHandler
 
 class TileEntityPedestal : TileEntity(){
-    private val inventory = ItemStackHandler(1)
+    init {
+        GameRegistry.registerTileEntity(this::class.java, ResourceLocation(MOD_ID, "tile_entity_pedestal"))
+    }
+
+    private val inventory = object : ItemStackHandler(1) {
+        override fun onContentsChanged(slot: Int) {
+            super.onContentsChanged(slot)
+            markDirty()
+        }
+    }
+
 
     override fun writeToNBT(compound: NBTTagCompound): NBTTagCompound {
         compound.setTag("inventory", inventory.serializeNBT())
