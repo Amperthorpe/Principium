cd17e40c3caab45a735195bf3e86a12fb3a3ba66
Alleluid <alleluid@users.noreply.github.com>
2019-01-14 23:41:08 -0800

    Made box harvestable with axe, pick

--- src/main/kotlin/com/alleluid/principium/common/blocks/box/BlockBox.kt
+++ src/main/kotlin/com/alleluid/principium/common/blocks/box/BlockBox.kt
@@ -1,10 +1,13 @@
 package com.alleluid.principium.common.blocks.box
 
 import com.alleluid.principium.ModGuiID
+import com.alleluid.principium.Utils.Formatting
 import com.alleluid.principium.common.blocks.BaseBlockTileEntity
 import net.minecraft.block.material.Material
 import net.minecraft.block.state.IBlockState
+import net.minecraft.client.util.ITooltipFlag
 import net.minecraft.entity.item.EntityItem
+import net.minecraft.item.ItemStack
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
@@ -14,6 +17,19 @@ import net.minecraftforge.items.IItemHandler
 object BlockBox : BaseBlockTileEntity<TileEntityBox>(Material.WOOD, ModGuiID.BOX, "block_box"){
     init {
         blockHardness = 3f
+    }
+
+    override fun addInformation(stack: ItemStack, worldIn: World?, tooltip: MutableList<String>, flagIn: ITooltipFlag) {
+        tooltip.add("${Formatting.LORE}Two chests in one full size block")
+        super.addInformation(stack, worldIn, tooltip, flagIn)
+    }
+
+    override fun isToolEffective(type: String, state: IBlockState): Boolean {
+        return when (type){
+            "pickaxe" -> true
+            "axe" -> true
+            else -> false
+        }
     }
 
     override fun isOpaqueCube(state: IBlockState): Boolean = false
