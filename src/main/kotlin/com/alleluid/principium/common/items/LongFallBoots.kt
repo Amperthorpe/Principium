9bed588683199603a0d3994f7cd89393416f9705
Alex Kaufman <milotig@gmail.com>
2019-05-01 17:24:52 -0700

    Add lore text to long fall boots.

--- src/main/kotlin/com/alleluid/principium/common/items/LongFallBoots.kt
+++ src/main/kotlin/com/alleluid/principium/common/items/LongFallBoots.kt
@@ -2,6 +2,8 @@ package com.alleluid.principium.common.items
 
 import com.alleluid.principium.MOD_ID
 import com.alleluid.principium.PrincipiumMod
+import com.alleluid.principium.Utils
+import net.minecraft.client.util.ITooltipFlag
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.inventory.EntityEquipmentSlot
 import net.minecraft.item.ItemArmor
@@ -14,6 +16,14 @@ class LongFallBootsBase(slot: EntityEquipmentSlot, val name: String) : ItemArmor
         translationKey = "$MOD_ID.$name"
         registryName = ResourceLocation(MOD_ID, name)
         creativeTab = PrincipiumMod.creativeTab
+    }
+
+    override fun addInformation(stack: ItemStack, worldIn: World?, tooltip: MutableList<String>, flagIn: ITooltipFlag) {
+        tooltip.add("${Utils.Formatting.LORE}We asked this test subject to just try to land on her head.")
+        tooltip.add("Negates fall damage")
+        tooltip.add("${Utils.Formatting.LORE}Heheh. She can't do it. Good job boots.")
+
+        super.addInformation(stack, worldIn, tooltip, flagIn)
     }
 
     override fun onArmorTick(world: World, player: EntityPlayer, itemStack: ItemStack) {
