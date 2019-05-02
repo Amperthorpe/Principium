523e426d2408996af3a41b2d593ed4058ec8d502
Alex Kaufman <milotig@gmail.com>
2019-05-01 18:29:51 -0700

    Remove unused BaseSword class.

--- src/main/kotlin/com/alleluid/principium/common/items/tools/PrincipicSword.kt
+++ src/main/kotlin/com/alleluid/principium/common/items/tools/PrincipicSword.kt
@@ -1,31 +1,14 @@
 package com.alleluid.principium.common.items.tools
 
-import com.alleluid.principium.MOD_ID
-import com.alleluid.principium.PrincipiumMod
 import com.alleluid.principium.Utils
 import com.alleluid.principium.common.items.BaseItem
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
-import net.minecraft.item.ItemSword
 import net.minecraft.util.EnumHand
-import net.minecraft.util.ResourceLocation
 import com.alleluid.principium.Utils.Formatting as ufm
 
-
-open class BaseSword(material: Item.ToolMaterial, val name: String) : ItemSword(material) {
-    init {
-        translationKey = "$MOD_ID.$name"
-        registryName = ResourceLocation(MOD_ID, name)
-        creativeTab = PrincipiumMod.creativeTab
-    }
-
-    fun registerItemModel() {
-        PrincipiumMod.proxy.registerItemRenderer(this, 0, name)
-    }
-}
 
 object PrincipicSword : BaseItem("sword_principic") {
     init {
