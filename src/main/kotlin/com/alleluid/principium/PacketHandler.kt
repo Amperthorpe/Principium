012421538595210965b500271b96c65ef294c162
Alex Kaufman <milotig@gmail.com>
2019-05-02 22:09:18 -0700

    Initial machine sync attempt

--- src/main/kotlin/com/alleluid/principium/PacketHandler.kt
+++ src/main/kotlin/com/alleluid/principium/PacketHandler.kt
@@ -1,12 +1,17 @@
 package com.alleluid.principium
 
+import com.alleluid.principium.common.blocks.smelter.TileEntitySmelter
 import com.alleluid.principium.common.items.weapons.BaseWeapon
 import com.alleluid.principium.common.items.weapons.WeaponPistol
 import io.netty.buffer.ByteBuf
+import net.minecraft.client.Minecraft
+import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.entity.player.EntityPlayerMP
 import net.minecraft.init.Items
 import net.minecraft.item.ItemStack
 import net.minecraft.util.EnumHand
+import net.minecraft.util.math.BlockPos
+import net.minecraftforge.energy.IEnergyStorage
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext
@@ -22,6 +27,7 @@ object PacketHandler {
     @JvmStatic
     fun registerMessages() {
         INSTANCE.registerMessage(ShootMessage.ShootMessageHandler::class.java, ShootMessage::class.java, uid++, Side.SERVER)
+        INSTANCE.registerMessage(MachineSyncMessage.MachineSyncMesssageHandler::class.java, MachineSyncMessage::class.java, uid++, Side.CLIENT)
     }
 }
 
@@ -47,9 +53,39 @@ class ShootMessage(var toSend: Float = 0f) : IMessage {
             }
             return null
         }
-
-
-
-
     }
+}
+
+class MachineSyncMessage(var energy: Int, var pos: BlockPos) : IMessage {
+    override fun toBytes(buf: ByteBuf?) {
+        buf?.writeInt(energy)
+        buf?.writeInt(pos.x)
+        buf?.writeInt(pos.y)
+        buf?.writeInt(pos.z)
+    }
+
+    override fun fromBytes(buf: ByteBuf?) {
+        energy = buf?.readInt() ?: -1
+        val x = buf?.readInt() ?: 0
+        val y = buf?.readInt() ?: 0
+        val z = buf?.readInt() ?: 0
+
+        pos = BlockPos(x, y, z)
+    }
+
+    class MachineSyncMesssageHandler : IMessageHandler<MachineSyncMessage, IMessage> {
+        override fun onMessage(message: MachineSyncMessage?, ctx: MessageContext?): IMessage? {
+            val player = Minecraft.getMinecraft().player
+            val energy = message?.energy ?: -2
+            val pos = message?.pos ?: BlockPos.ORIGIN
+            Minecraft.getMinecraft().addScheduledTask {
+                val TE = player.world.getTileEntity(pos)
+                if (TE is IEnergyStorage && TE is TileEntitySmelter){
+                    TE.energy = energy
+                }
+            }
+            return null
+        }
+    }
+
 }
