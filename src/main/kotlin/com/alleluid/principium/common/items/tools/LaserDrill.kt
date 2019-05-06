a437982aa507f8981626f375cb5a8dca8e80cb95
Alex Kaufman <milotig@gmail.com>
2019-05-06 14:31:17 -0700

    Refactor laserDrill;
    Has right click weapon, is no longer a sub of itemPickaxe, now has custom attack damage and speed

--- src/main/kotlin/com/alleluid/principium/common/items/tools/LaserDrill.kt
+++ src/main/kotlin/com/alleluid/principium/common/items/tools/LaserDrill.kt
@@ -15,31 +15,63 @@ import net.minecraft.util.math.Vec3d
 import net.minecraft.world.World
 import com.alleluid.principium.GeneralUtils.Formatting as umf
 import com.alleluid.principium.GeneralUtils.ifClient
+import com.alleluid.principium.GeneralUtils.replaceModifer
+import com.alleluid.principium.GeneralUtils.spawnParticleVec
+import com.alleluid.principium.common.items.BaseItem
+import com.google.common.collect.Multimap
 import net.minecraft.block.BlockShulkerBox
 import net.minecraft.enchantment.Enchantment
+import net.minecraft.enchantment.EnumEnchantmentType
+import net.minecraft.entity.SharedMonsterAttributes
+import net.minecraft.entity.ai.attributes.AttributeModifier
+import net.minecraft.entity.ai.attributes.IAttributeInstance
 import net.minecraft.entity.player.EntityPlayerMP
 import net.minecraft.init.Enchantments
+import net.minecraft.inventory.EntityEquipmentSlot
+import net.minecraft.item.Item
 import net.minecraft.tileentity.TileEntityShulkerBox
 import net.minecraft.util.EnumParticleTypes
 import net.minecraft.util.math.RayTraceResult
 import net.minecraft.world.WorldServer
 import net.minecraftforge.common.util.Constants
 
-object LaserDrill : ItemPickaxe(PrincipiumMod.principicToolMaterial) {
+object LaserDrill : BaseItem("laser_drill") {
     private const val directDrops = true
     private const val weaponTraceDist = 100.0
 
     init {
-        creativeTab = PrincipiumMod.creativeTab
-        registryName = ResourceLocation(MOD_ID, "laser_drill")
-        translationKey = "$MOD_ID.laser_drill"
+        loreText.add("This drill is the drill that will pierce the heavens!")
+        infoText.add("Left click to burst mine, right click is a laser blast.")
+        infoText.add("Shift right click will switch to Precise Mode.")
+        infoText.add("")
     }
 
-    fun registerItemModel() = PrincipiumMod.proxy.registerItemRenderer(this, 0, "laser_drill")
+    override fun getAttributeModifiers(slot: EntityEquipmentSlot, stack: ItemStack): Multimap<String, AttributeModifier> {
+        val map = super.getAttributeModifiers(slot, stack)
+        if (slot == EntityEquipmentSlot.MAINHAND) {
+            map.put(SharedMonsterAttributes.ATTACK_DAMAGE.name,
+                    AttributeModifier(ATTACK_DAMAGE_MODIFIER,
+                            SharedMonsterAttributes.ATTACK_DAMAGE.name,
+                            13.0, 0
+                    )
+            )
+            map.put(SharedMonsterAttributes.ATTACK_SPEED.name,
+                    AttributeModifier(ATTACK_SPEED_MODIFIER,
+                            SharedMonsterAttributes.ATTACK_SPEED.name,
+                            -4.0, 0
+                    )
+            )
+        }
+        return map
+    }
 
     override fun getDestroySpeed(stack: ItemStack, state: IBlockState): Float = 1600f
     override fun canItemEditBlocks(): Boolean = true // It can edit any block, it's a laser drill.
     override fun canHarvestBlock(state: IBlockState, stack: ItemStack): Boolean = true
+    override fun canApplyAtEnchantingTable(stack: ItemStack, enchantment: Enchantment): Boolean {
+        return enchantment.type == EnumEnchantmentType.WEAPON || enchantment.type == EnumEnchantmentType.DIGGER
+                || super.canApplyAtEnchantingTable(stack, enchantment)
+    }
 
     fun onPrecisionMine(worldIn: WorldServer, playerIn: EntityPlayerMP) {
         val dist = playerIn.getEntityAttribute(EntityPlayer.REACH_DISTANCE).attributeValue
@@ -74,7 +106,7 @@ object LaserDrill : ItemPickaxe(PrincipiumMod.principicToolMaterial) {
             val block = state.block
 
             // Special handling for shulker boxes; add to drops then ensure no dropping as item
-            if (block is BlockShulkerBox){
+            if (block is BlockShulkerBox) {
                 dropsList.add(block.getItem(worldIn, pos, state))
                 val shulker = worldIn.getTileEntity(pos) as TileEntityShulkerBox
                 shulker.isDestroyedByCreativePlayer = true
@@ -96,12 +128,6 @@ object LaserDrill : ItemPickaxe(PrincipiumMod.principicToolMaterial) {
         }
     }
 
-    override fun addInformation(stack: ItemStack, worldIn: World?, tooltip: MutableList<String>, flagIn: ITooltipFlag) {
-        tooltip.add("${umf.LORE}This drill is the drill that will pierce the heavens!")
-        tooltip.add("Left click to burst mine, right click is single block Silk Touch.")
-        tooltip.add("Shift right click will switch to Precise Mode.")
-    }
-
     override fun onUpdate(stack: ItemStack, worldIn: World, entityIn: Entity, itemSlot: Int, isSelected: Boolean) {
         // Ensures this item always has an NBT tag.
         if (stack.tagCompound == null) {
@@ -109,12 +135,13 @@ object LaserDrill : ItemPickaxe(PrincipiumMod.principicToolMaterial) {
                 setBoolean("preciseMode", false)
             }
         }
+
         super.onUpdate(stack, worldIn, entityIn, itemSlot, isSelected)
     }
 
     override fun onItemRightClick(worldIn: World, playerIn: EntityPlayer, handIn: EnumHand): ActionResult<ItemStack> {
         val stack = playerIn.getHeldItem(handIn)
-        if (!playerIn.isSneaking){
+        if (!playerIn.isSneaking) {
             // Weapon
             val look = playerIn.lookVec
             val eyePos = playerIn.getPositionEyes(1f)
@@ -123,22 +150,36 @@ object LaserDrill : ItemPickaxe(PrincipiumMod.principicToolMaterial) {
             val entTrace = JavaUtils.getEntityIntercept(worldIn, playerIn, eyePos, look, endPoint, null)
 
             val blockTrace = worldIn.rayTraceBlocks(eyePos, entTrace?.hitVec ?: endPoint)
-            val effectsEndPoint = if (entTrace != null){
-                if (blockTrace == null || blockTrace.typeOfHit == RayTraceResult.Type.MISS) {
-                    val hitPos = entTrace.hitVec
-                    val entPos = entTrace.entityHit.positionVector
-                    val whoKnows = entPos.subtract(hitPos).normalize()
-                    entTrace.entityHit.setVelocity(-whoKnows.x, -whoKnows.y, -whoKnows.z)
-                    hitPos
-                } else { blockTrace.hitVec }
+            val effectsEndPoint = if (entTrace != null) {
+                if (blockTrace == null || blockTrace.typeOfHit == RayTraceResult.Type.MISS
+                        || !worldIn.getBlockState(blockTrace.blockPos).isFullCube) {
+                    playerIn.attackTargetEntityWithCurrentItem(entTrace.entityHit)
+                    entTrace.hitVec
+                } else {
+                    blockTrace.hitVec
+                }
 
-            } else { blockTrace?.hitVec }
+            } else {
+                blockTrace?.hitVec
+            }
 
-            if (worldIn.isRemote && effectsEndPoint != null){
-                worldIn.spawnParticle(EnumParticleTypes.CRIT,
+            if (worldIn.isRemote && effectsEndPoint != null) {
+                worldIn.spawnParticle(EnumParticleTypes.EXPLOSION_LARGE,
                         effectsEndPoint.x, effectsEndPoint.y, effectsEndPoint.z,
                         0.0, 0.0, 0.0
                 )
+                for (i in 1..(eyePos.distanceTo(effectsEndPoint) * 5).toInt()) {
+                    val lookEye = look.add(eyePos)
+                    worldIn.spawnParticle(EnumParticleTypes.REDSTONE,
+                            true,
+                            lookEye.x + (i / 5 * look.x),
+                            lookEye.y + (i / 5 * look.y),
+                            lookEye.z + (i / 5 * look.z),
+                            worldIn.rand.nextGaussian() * 0.1,
+                            worldIn.rand.nextGaussian() * 0.1,
+                            worldIn.rand.nextGaussian() * 0.1
+                    )
+                }
             }
 
         } else {
