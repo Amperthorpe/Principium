65b1e3c2a581b85a4acf62ca4bf58623582ae340
Alex Kaufman <milotig@gmail.com>
2019-05-11 21:58:51 -0700

    Work towards substructs

--- src/main/kotlin/com/alleluid/principium/common/items/basic/ItemSubstruct.kt
+++ src/main/kotlin/com/alleluid/principium/common/items/basic/ItemSubstruct.kt
@@ -4,12 +4,21 @@ import com.alleluid.principium.PrincipiumMod
 import com.alleluid.principium.common.items.BaseItem
 
 object ItemSubstruct : BaseItem("substruct") {
+    init {
+        maxDamage = 0
+        hasSubtypes = true
+        setNoRepair()
+    }
+
     val variants: List<String> = listOf(
             "basic",
             "iron",
             "golden",
             "diamond"
     )
+
+    override fun isDamageable() = false
+
 
     override fun registerItemModel() {
         var idx = 0
