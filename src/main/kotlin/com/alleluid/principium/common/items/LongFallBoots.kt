3ed64ae4332cf563a3f55987860fc3e7b44cb921
Alex Kaufman <milotig@gmail.com>
2019-05-25 14:54:02 -0700

    Refactor of utils and advice from MMD

--- src/main/kotlin/com/alleluid/principium/common/items/LongFallBoots.kt
+++ src/main/kotlin/com/alleluid/principium/common/items/LongFallBoots.kt
@@ -2,7 +2,8 @@ package com.alleluid.principium.common.items
 
 import com.alleluid.principium.MOD_ID
 import com.alleluid.principium.PrincipiumMod
-import com.alleluid.principium.GeneralUtils
+import com.alleluid.principium.util.Formatting
+import net.minecraft.client.resources.I18n
 import net.minecraft.client.util.ITooltipFlag
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.inventory.EntityEquipmentSlot
@@ -19,9 +20,8 @@ class LongFallBootsBase(slot: EntityEquipmentSlot, val name: String) : ItemArmor
     }
 
     override fun addInformation(stack: ItemStack, worldIn: World?, tooltip: MutableList<String>, flagIn: ITooltipFlag) {
-        tooltip.add("${GeneralUtils.Formatting.LORE}We asked this test subject to just try and land on her head.")
-        tooltip.add("Negates fall damage")
-        tooltip.add("${GeneralUtils.Formatting.LORE}Heheh. She can't do it. Good work boots.")
+        tooltip.add(Formatting.LORE + I18n.format("info.$MOD_ID.$name.lore1")) //We do what we must because we can.
+        tooltip.add(I18n.format("info.$MOD_ID.$name.info1")) //Negates fall damage.
 
         super.addInformation(stack, worldIn, tooltip, flagIn)
     }
