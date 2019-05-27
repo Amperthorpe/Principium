7dd14169568422fa7926b5f0ececf63339e99c9e
Alex Kaufman <milotig@gmail.com>
2019-05-26 17:51:24 -0700

    Rename smelter to smelterTile to be more clear on type

--- src/main/kotlin/com/alleluid/principium/common/blocks/smelter/ContainerSmelter.kt
+++ src/main/kotlin/com/alleluid/principium/common/blocks/smelter/ContainerSmelter.kt
@@ -2,35 +2,29 @@ package com.alleluid.principium.common.blocks.smelter
 
 import com.alleluid.principium.common.BaseContainer
 import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.entity.player.InventoryPlayer
-import net.minecraft.inventory.Container
-import net.minecraft.inventory.IContainerListener
-import net.minecraft.inventory.IInventory
-import net.minecraft.item.ItemStack
 import net.minecraft.util.EnumFacing
-import net.minecraft.util.NonNullList
 import net.minecraftforge.items.CapabilityItemHandler
 import net.minecraftforge.items.SlotItemHandler
 
-class ContainerSmelter(val player: EntityPlayer, val smelter: TileEntitySmelter) : BaseContainer(player.inventory, smelter) {
+class ContainerSmelter(val player: EntityPlayer, val smelterTile: TileEntitySmelter) : BaseContainer(player.inventory, smelterTile) {
     val energy
-        get() = smelter.energyStored
+        get() = smelterTile.energyStored
 
     init {
-        val inventory = smelter.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH)
+        val inventory = smelterTile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH)
         addSlotToContainer(object : SlotItemHandler(inventory, 0, 77, 20) {
             override fun onSlotChanged() {
-                smelter.markDirty()
+                smelterTile.markDirty()
             }
         })
         addSlotToContainer(object : SlotItemHandler(inventory, 1, 77, 57) {
             override fun onSlotChanged() {
-                smelter.markDirty()
+                smelterTile.markDirty()
             }
         })
         addSlotToContainer(object : SlotItemHandler(inventory, 2, 124, 38) {
             override fun onSlotChanged() {
-                smelter.markDirty()
+                smelterTile.markDirty()
             }
         })
 
